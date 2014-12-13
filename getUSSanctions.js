190ba4dcdc6d426b4afc04792c89313ec959b742
rorygreig <rorygreig@gmail.com>
2014-12-13 11:34:25 +0000

    filter out companies from US list

--- getUSSanctions.js
+++ getUSSanctions.js
@@ -10,6 +10,12 @@ var getTargetsQuery = "select * from xml where url='" + url + "'";
 console.log(getTargetsQuery);
 
 new yql.exec(getTargetsQuery, function(response) {
-  console.log(response.query.results.sdnList);
+  // console.log(response.query.results.sdnList.sdnEntry);
+  var targets = response.query.results.sdnList.sdnEntry;
+  targets = targets.filter(function(target){
+    return target.sdnType.indexOf('Individual') > -1;
+  });
+
+  console.log(targets[0].akaList);
 
 });
