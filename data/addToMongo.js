22a013d3c0a591b270a019c8033319b063c0d99f
Stewart Douglas <stewart.douglas13@imperial.ac.uk>
2014-12-13 12:18:43 +0000

    Switched to camelCase

---
+++ data/addToMongo.js
@@ -0,0 +1,24 @@
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect("mongodb://localhost:27017/kyc",function(err,db){
+	if(err){
+		console.log('Error connecting to Mongo');
+	} else {
+		console.log('Successfully connected to Mongo');
+		
+		require("./UK_MPs/uk_mps.json").forEach(function(element){
+			db.collection('mps').insert(element,{w:0});			
+		});
+
+		require("./US_Sanctions/UStargets.json").forEach(function(element){
+			db.collection('usSanctions').insert(element,{w:0});
+		});
+
+/*
+		require("./Data/UK_Sanctions/UKtargets.json").forEach(function(element){
+			db.collection('ukSanctions').insert(element,{w:0});
+		});		
+*/
+
+	}
+});
