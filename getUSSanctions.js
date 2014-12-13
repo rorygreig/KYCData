860fd0f3c9d4e6eb439a5a0bd94e2aba1723b9e9
rorygreig <rorygreig@gmail.com>
2014-12-13 11:57:31 +0000

    save US targets to JSON

--- getUSSanctions.js
+++ getUSSanctions.js
@@ -1,4 +1,5 @@
 var yql = require('yql');
+var fs = require('fs');
 
 var targets = Array();
 
@@ -6,16 +7,47 @@ var url = "http://www.treasury.gov/ofac/downloads/consolidated/consolidated.xml"
 
 var getTargetsQuery = "select * from xml where url='" + url + "'";
 
+var results = Array();
 
 console.log(getTargetsQuery);
 
 new yql.exec(getTargetsQuery, function(response) {
   // console.log(response.query.results.sdnList.sdnEntry);
   var targets = response.query.results.sdnList.sdnEntry;
-  targets = targets.filter(function(target){
+  individuals = targets.filter(function(target){
     return target.sdnType.indexOf('Individual') > -1;
   });
 
-  console.log(targets[0].akaList);
+  // create results array
+  individuals.forEach(function(target){
+    var result = Object();
+    result.entity_type = target.sdnType;
+    result.first_name = target.firstName;
+    result.last_name = target.lastName;
+    result.nationality = "";
+    result.data_source = "US Sanctions List";
+    result.list_type = "Watchlist";
+    result.addressList = target.addressList;
+    result.aka_list = target.akaList;
+
+    results.push(result);
+  })
+
+  console.log(results);
+
+  //save results to JSON
+  saveToJSON(results, './UStargets.json');
 
 });
+
+function saveToJSON(targets, fileName){
+
+  fs.appendFile(fileName, JSON.stringify(targets, null, 4), function(err) {
+      if(err) {
+        console.log(err);
+      } else {
+        console.log("JSON saved to " + fileName);
+      }
+  });
+
+}
