15fb44ff5b2bd1ae23ced9781b3e44dbc805af1c
rorygreig <rorygreig@gmail.com>
2014-12-13 12:33:31 +0000

    add UK targets nationalities

--- getUKSanctions.js
+++ getUKSanctions.js
@@ -1,5 +1,4 @@
 var yql = require('yql');
-var fs = require('fs');
 
 var results = Array();
 
@@ -14,7 +13,7 @@ console.log(getTargetsQuery);
 new yql.exec(getTargetsQuery, function(response) {
   // console.log(response.query.results.results.li);
   targets = response.query.results.results.li;
-  console.log(typeof(targets));
+  // console.log(targets);
 
   var individuals = targets.filter(function isIndividual(target){
     return target.strong.indexOf('Organisation Name') == -1;
@@ -23,13 +22,20 @@ new yql.exec(getTargetsQuery, function(response) {
   // create results array
   individuals.forEach(function(target){
     var result = Object();
+    var fields = target.p.strong;
+    var nationalityIndex = fields.indexOf('Nationality:');
+
     var details = target.p.content;
     details = details.split("\n");
 
     result.entityType = 'Individual';
     result.firstName = details[1].trim();
     result.lastName = details[0].trim();
-    result.nationality = "";
+    if(nationalityIndex > -1){
+      result.nationality = details[nationalityIndex + 2].trim();
+    } else {
+      result.nationality = "";
+    }
     result.dataSource = "UK Sanctions List";
     result.listType = "Watchlist";
     result.addressList = "";
@@ -38,9 +44,9 @@ new yql.exec(getTargetsQuery, function(response) {
     results.push(result);
   });
 
-  console.log(results.length)
+  console.log(results);
+  console.log(results.length);
 
-  //save results to JSON
   saveToJSON(results, './UKtargets.json');
 
 });
