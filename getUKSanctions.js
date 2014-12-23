aff74c9efa0d4e100714910b0797f6878dcdf011
rorygreig <rorygreig@gmail.com>
2014-12-23 18:06:28 +0000

    extracting name and position for targets

--- getUKSanctions.js
+++ getUKSanctions.js
@@ -7,7 +7,6 @@ var url = "http://hmt-sanctions.s3.amazonaws.com/sanctionsconlist.htm";
 // var getTargetsQuery = "select * from html where url='" + url + "' and xpath='//li'";
 var getTargetsQuery = "select * from data.html.cssselect where url='" + url + "' AND css='.SpacedOut'";
 
-
 console.log(getTargetsQuery);
 
 new yql.exec(getTargetsQuery, function(response) {
--- getWorldLeaders.js
+++ getWorldLeaders.js
@@ -16,7 +16,9 @@ countryCodes.forEach(function(country){
 var queries = Array();
 
 urls.forEach(function(url){
-  var query = "select * from data.html.cssselect where url='" + url + "' AND css='.list-style'";
+  // select * from html where url="https://www.cia.gov/library/publications/world-leaders-1/AF.html" and compat="html5" and xpath='//*[@id="cosContent1"]/div[4]/div/div/ul'
+
+  var query = "select * from html where url='" + url + "' and compat='html5' and xpath='//*[@id=\"cosContent1\"]/div[4]/div/div/ul'";
   queries.push(query);
 });
 
@@ -24,12 +26,26 @@ urls.forEach(function(url){
 console.log(queries[0]);
 
 new yql.exec(queries[0], function(response) {
-  console.log(response.query.results.results);
-  var results = response.query.results.results;
+  var results = response.query.results.ul.li;
+  var targets = Array();
+  // console.log(results);
 
-  saveToJSON(results, './data/CIAWorldLeaders/worldLeaders.json');
+  results.forEach(function(item){
+    targets.push(extractTarget(item));
+  });
+
+  saveToJSON(targets, './data/CIAWorldLeaders/targets.json');
 
 });
+
+function extractTarget(listItem){
+  var target = Object();
+  var td = listItem.table.tbody.tr.td.table.tbody.tr.td;
+  target.position = td[0].span.span.content;
+  target.name = td[1].span.span.content;
+  console.log(target);
+  return(target);
+}
 
 function saveToJSON(targets, fileName){
 
