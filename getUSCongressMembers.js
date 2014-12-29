92de0450929b5730cc060a78568a9b2e2f448ec7
rorygreig <rorygreig@gmail.com>
2014-12-29 13:47:52 +0000

    select all congress members from same url

--- getUSCongressMembers.js
+++ getUSCongressMembers.js
@@ -2,44 +2,15 @@ var yql = require('yql');
 var fs = require('fs');
 var async = require('async');
 
-var targets = Array();
+var url = "https://www.congress.gov/members"
+var getMembersQuery = "select * from html where url='" + url + "' and compat='html5' and xpath='//*[@id=\"content\"]/div'";
 
-var pages = [1,2,3];
+console.log(getMembersQuery);
 
-var queryFuncs = Array();
-
-var memberResults = Array();
-
-pages.forEach(function(page){
-
-  // var url = "https://www.congress.gov/members?pageSize=250&q=%7B\"congress\"%3A\"113\"%7D&page=\"" + page + "\"";
-  var url = "https://www.congress.gov/members?pageSize=250&q=%7B%22congress%22%3A%22113%22%7D&page=%22" + page + "%22"
-  var getMembersQuery = "select * from html where url='" + url + "'";
-
-  var results = Array();
-
-  console.log(getMembersQuery);
-
-  queryFuncs.push(function(callback){
-    new yql.exec(getMembersQuery, function(response) {
-      console.log(response.query.results);
-      // if(response.query.results !== null){
-      //   console.log(response.query.results.body);
-      //   var results = response.query.results.body;
-      //   var members = Array();
-      //
-      //   memberResults = memberResults.concat(results);
-      // }
-      callback();
-    });
-  });
-
-});
-
-async.parallel(queryFuncs, function(){
-  //save concatenated results here
-  console.log(memberResults);
-  // saveToJSON(memberResults, './data/USCongressMembers/USCongressMembers.json');
+new yql.exec(getMembersQuery, function(response) {
+  console.log(response.query.results);
+  var targets = response.query.results
+  saveToJSON(targets, './data/USCongressMembers/USCongressMembers.json');
 });
 
 function saveToJSON(targets, fileName){
