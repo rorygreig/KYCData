ff12f0d8ceb9bac9f93e18582eead1d7a02fb025
rorygreig <rorygreig@gmail.com>
2014-12-29 14:07:05 +0000

    completed list of senators and representatives

--- getUSCongressMembers.js
+++ getUSCongressMembers.js
@@ -2,19 +2,32 @@ var yql = require('yql');
 var fs = require('fs');
 var async = require('async');
 
-var url = "https://www.congress.gov/members"
-var getMembersQuery = "select * from html where url='" + url + "' and compat='html5' and xpath='//*[@id=\"content\"]/div'";
+var url = "https://www.congress.gov/members";
 
-console.log(getMembersQuery);
+var getRepresentativesQuery = "select * from html where url='" + url + "' and compat='html5' and xpath='//*[@id=\"members-representatives\"]/option'";
+var getSenatorsQuery = "select * from html where url='" + url + "' and compat='html5' and xpath='//*[@id=\"members-senators\"]/option'";
 
-new yql.exec(getMembersQuery, function(response) {
-  console.log(response.query.results);
-  var targets = response.query.results
-  saveToJSON(targets, './data/USCongressMembers/USCongressMembers.json');
+console.log(getRepresentativesQuery);
+
+//get members of house of representatives
+new yql.exec(getRepresentativesQuery, function(response) {
+  var representatives = response.query.results.option;
+  representatives.shift();
+  console.log(representatives);
+  console.log(representatives.length);
+  saveToJSON(representatives, './data/USCongressMembers/USRepresentatives.json');
+});
+
+//get members of the senat
+new yql.exec(getSenatorsQuery, function(response) {
+  var senators = response.query.results.option;
+  senators.shift();
+  console.log(senators);
+  console.log(senators.length);
+  saveToJSON(senators, './data/USCongressMembers/USSenators.json');
 });
 
 function saveToJSON(targets, fileName){
-
   fs.writeFile(fileName, JSON.stringify(targets, null, 4), function(err) {
     if(err) {
       console.log(err);
