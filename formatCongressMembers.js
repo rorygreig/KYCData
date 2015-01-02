59949ff53ea6e2823e87ad21e925853ce5073af3
rorygreig <rorygreig@gmail.com>
2015-01-02 12:38:59 +0000

    names fully formatted

--- formatCongressMembers.js
+++ formatCongressMembers.js
@@ -1,11 +1,21 @@
+var fs = require('fs');
+
 var representatives = require('./data/US_CongressMembers/USRepresentatives.json');
 var senators = require('./data/US_CongressMembers/USSenators.json');
 
-console.log(extractValues(representatives[0].content));
+// console.log(extractValues(representatives[0].content));
+
+saveToJSON(formatResults(representatives, "representatives"), './data/US_CongressMembers/USRepresentativesFormatted.json');
+saveToJSON(formatResults(senators, "senators"), './data/US_CongressMembers/USSenatorsFormatted.json');
+
+// console.log(representatives);
+// representatives = formatResults(representatives, 'representatives');
+
 
 function formatResults(members, house){
-  var members = Array();
   var results = Array();
+
+  console.log(members);
 
   members.forEach(function(member){
     results.push(extractValues(member.content));
@@ -19,8 +29,22 @@ function extractValues(nameStr){
   values = nameStr.split(" ");
   result.party = values.pop();
   console.log(values);
-  result.lastName = values.shift();
+  result.lastName = values.shift().replace(',', ' ').trim();
   //TODO: use array reduce function to fold firstName array into one String
-  result.firstName = values;
+  result.firstName = values.reduce(function(accumulator, currentValue, index, array) {
+    return accumulator + ' ' + currentValue.replace(',', ' ').trim();
+  });;
   return result;
+}
+
+
+function saveToJSON(targets, fileName){
+  fs.writeFile(fileName, JSON.stringify(targets, null, 4), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("JSON saved to " + fileName);
+    }
+  });
+
 }
